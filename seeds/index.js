15ceca595ff1a43e4bac79c6c07d92d2f375e734
realjameswang <163924782+realjameswang@users.noreply.github.com>
2025-09-22 00:21:39 -0400

    add seeder helper function

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,14 @@
-const mongoose = require('mongoose')
-const Campground = require('../models/campground')
-const cities = require ('./cities')
+// populate the database with sample data for testing
+// so that I can test my app with realistic-looking entries
 
+
+//imports and setup
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require ('./cities');
+const {places, descriptors} = require('./seedHelpers');
+
+//connect to mongoDB
 mongoose.connect('mongodb://127.0.0.1/yelp-camp', {}).then(() => {
     console.log("MONGO Connection Open!!")
 })
@@ -10,21 +17,31 @@ mongoose.connect('mongodb://127.0.0.1/yelp-camp', {}).then(() => {
     console.log(err)
 })
 
+//optional event logging
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected")
 })
 
+// helper function: pick a random element from an array
+// generate random campground titles
+const sample = array => array[Math.floor(Math.random() * array.length)]
+
+
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i =0; i <50; i++) {
+    await Campground.deleteMany({}); // clears all existing campground data
+    for (let i =0; i <50; i++) { //create 50 new campground entries 
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`, // randome city state
+            title: `${sample(descriptors)} ${sample(places)}` // random combo of descriptor and place
         })
         await camp.save();
     }
 }
 
-seedDB();
+// run the seeder and close the connection
+seedDB().then(() => {
+    mongoose.connection.close()
+});
